06293f76e976d4d31872d446d382ee7c98bba382
ritik srivastava <ritiksrivastava025@gmail.com>
2019-10-01 15:54:33 +0530

    date formatting

--- models/BloodRequest.js
+++ models/BloodRequest.js
@@ -4,14 +4,14 @@ const BloodRequestSchema = new mongoose.Schema({
 	name: { type: String, required: true },
     bloodGroup: { type: String, required: true },
     phone: { type: String, required: true },
-    dob: { type: Date, required: true },
+    dob: { type: String, required: true },
     address: { type: String, required: true },
     pincode: { type: Number, required: true },
     requestFor: { type: String, enum: ['self', 'other'] },
     require: { type: String, required: true, enum: ['platelets', 'blood'] },
     units: { type: Number, required: true },
     reason: { type: String },
-    neededBy: { type: Date, required: true },
+    neededBy: { type: String, required: true },
     timings: { type: String },
 }, { timestamps: true }
 );
